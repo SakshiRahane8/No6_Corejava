0b03e099cfc7a5b8122b1712a3c8a4be3daf4ece
sigma <sigma@LAPTOP-NT36M23P>
2023-07-21 13:57:39 +0530

    Second commit

---
+++ dayone/src/org/tnsif/operators/BitwiseOperatorDemo.java
@@ -0,0 +1,16 @@
+//Program to demonstrate bitwise operator
+package org.tnsif.operators;
+
+public class BitwiseOperatorDemo {
+
+	public static void main(String[] args) {
+		int x=13, y=9;
+		System.out.println(x&y);
+		System.out.println(x|y);
+		System.out.println(x^y);
+		System.out.println(8>>1);         //8*(2^1)
+		System.out.println(8<<1);         //8/(2^1)
+
+	}
+
+}
---
+++ dayone/src/org/tnsif/operators/TernaryOperatorDemo.java
@@ -0,0 +1,17 @@
+//Program to demonstrate ternary operator
+package org.tnsif.operators;
+
+import java.util.Scanner;
+
+public class TernaryOperatorDemo {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the value of num: ");
+		int num=s.nextInt();
+		String Result = (num%2==0)?"Even": "Odd";
+		System.out.println("Result is: "+Result);
+		
+	}
+
+}
