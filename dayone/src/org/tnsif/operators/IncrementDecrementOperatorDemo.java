02d9df014ba31240aafca8bd8c6781e266712cb0
sigma <sigma@LAPTOP-NT36M23P>
2023-07-24 22:27:35 +0530

    First commit

--- dayone/src/org/tnsif/operators/IncrementDecrementOperatorDemo.java
+++ dayone/src/org/tnsif/operators/IncrementDecrementOperatorDemo.java
@@ -7,15 +7,15 @@ public class IncrementDecrementOperatorDemo {
 		int x=23, y=6;
 		int res1= x++ + ++y; //23+7
 		System.out.println(res1); //30
-		System.out.println(x);    //
-		System.out.println(y);
+		System.out.println(x);    //24
+		System.out.println(y);    //7
 		
 		System.out.println("Result two");
 		
-		int res2= x-- + --y; 
-		System.out.println(res2);//24+6
+		int res2= x-- + --y; //24+6
+		System.out.println(res2);//24+6=30
 		System.out.println(x);   //23
-		System.out.println(y);   //
+		System.out.println(y);   //6
 	}
 
 }
